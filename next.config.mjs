6182babb5493f35eecb4a9d71d2ff50aa7abb61d
san hanityo <san.hanityo@salt.co.id>
2024-04-19 18:14:21 +0700

    chore: setup next image remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
