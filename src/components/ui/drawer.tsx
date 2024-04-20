dc55895893e21e360853793036e38ff3e2a5f097
san hanityo <san.hanityo@salt.co.id>
2024-04-20 13:34:09 +0700

    fix(drawer): transition with framer motion

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -1,29 +1,9 @@
-import { cn } from "@/lib/utils";
-import { cva } from "class-variance-authority";
+import { AnimatePresence, LazyMotion, m } from "framer-motion";
 import { XIcon } from "lucide-react";
 import Image from "next/image";
 
-const DrawerTopVariant = cva(
-  "fixed h-12 w-full bg-gradient-down from-primary to-primary-darken top-0 z-50 -translate-y-full transition-transform duration-700",
-  {
-    variants: {
-      open: {
-        true: "translate-y-0",
-      },
-    },
-  },
-);
-
-const DrawerContentVariant = cva(
-  "fixed h-[calc(100vh-48px)] w-full bg-white bottom-0 z-50 translate-y-full items-center transition-transform duration-700 overflow-scroll pb-20",
-  {
-    variants: {
-      open: {
-        true: "translate-y-0",
-      },
-    },
-  },
-);
+const loadFeature = () =>
+  import("@/lib/dom-animation").then((res) => res.default);
 
 export const Drawer = ({
   children,
@@ -35,28 +15,54 @@ export const Drawer = ({
   setOpen: (value: boolean) => void;
 }) => {
   return (
-    <div>
-      <div className={cn(DrawerTopVariant({ open }))}>
-        <div className="container flex h-12 items-center">
-          <a href="/" className="w-6/12 lg:w-2/12">
-            <Image
-              src="/images/ontego-bussines-mobility.svg"
-              width={165}
-              height={22}
-              alt="ontego business mobility"
-            />
-          </a>
-        </div>
-      </div>
-      <div className={cn(DrawerContentVariant({ open }))}>
-        <button
-          className="absolute right-10 top-10"
-          onClick={() => setOpen(false)}
-        >
-          <XIcon />
-        </button>
-        <div className="container-sm pt-[100px]">{children}</div>
-      </div>
-    </div>
+    <AnimatePresence initial={false}>
+      {open && (
+        <LazyMotion features={loadFeature}>
+          <m.div
+            className="fixed top-0 z-50 h-12 w-full bg-gradient-down from-primary to-primary-darken"
+            initial="hidden"
+            animate="open"
+            exit="hidden"
+            variants={{
+              open: { y: 0 },
+              hidden: { y: "-100%" },
+            }}
+            transition={{ duration: 0.7 }}
+            key={"drawer-top"}
+          >
+            <div className="container flex h-12 items-center">
+              <a href="/" className="w-6/12 lg:w-2/12">
+                <Image
+                  src="/images/ontego-bussines-mobility.svg"
+                  width={165}
+                  height={22}
+                  alt="ontego business mobility"
+                />
+              </a>
+            </div>
+          </m.div>
+          <m.div
+            className="fixed bottom-0 z-50 h-[calc(100vh-48px)] w-full items-center overflow-scroll bg-white pb-20"
+            initial="hidden"
+            animate="open"
+            exit="hidden"
+            variants={{
+              open: { y: 0 },
+              hidden: { y: "100%" },
+            }}
+            transition={{ duration: 0.7 }}
+            key={"drawer-bottom"}
+          >
+            <button
+              className="absolute right-10 top-10"
+              onClick={() => setOpen(false)}
+            >
+              <XIcon />
+            </button>
+            <div className="container-sm pt-[100px]">{children}</div>
+          </m.div>
+        </LazyMotion>
+      )}
+    </AnimatePresence>
   );
 };
